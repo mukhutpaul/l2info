dfc8733bdb0bc0c4e07abec3b68245949f5fd3ae
mukhut <mukhutjp@gmail.com>
2025-02-19 16:57:19 +0400

    MODIF

--- app_l2info/admin.py
+++ app_l2info/admin.py
@@ -3,5 +3,6 @@ from .models import Promotion,Etudiant
 
 # Register your models here.
 
+admin.site.site_header("GESTION DES ETUDIANT")
 admin.site.register(Promotion,list_display=('codeprom','libelle'))
 admin.site.register(Etudiant,list_display=('matricule','nom','postnom','promotion'),search_fields=['matricule','nom','postnom'])
